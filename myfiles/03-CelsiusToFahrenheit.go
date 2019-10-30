0124800baac99e4fe0cd5e27afa6fe81dbb22558
Chris Tham <chris.tham@hellotham.com>
2019-10-30 15:47:34 +1100

    Weather Workshop

--- myfiles/03-CelsiusToFahrenheit.go
+++ myfiles/03-CelsiusToFahrenheit.go
@@ -7,7 +7,7 @@ func main() {
 	fahrenheit := celsius*9/5 + 32
 	fmt.Printf("%.2f celsius to fahrenheit %.2f\n", celsius, fahrenheit)
 
-	celsiusArray := []float64{88, 32, 0, 38}
+	celsiusArray := [...]float64{88, 32, 0, 38}
 	for _, v := range celsiusArray {
 		var celsiusItem = v
 		fmt.Printf("%.2f celsius to fahrenheit %.2f\n", celsiusItem, celsiusItem*9/5+32)
